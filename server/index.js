2af8d7ce24fad694c5f7f2ee40be0a26b8179dc6
Gregor <gregor@martynus.net>
2016-08-07 17:21:52 -0700

    refactor: removed files now living in @hoodie/server

--- server/index.js
+++ server/index.js
@@ -4,13 +4,15 @@ module.exports.register.attributes = {
 }
 
 var corsHeaders = require('hapi-cors-headers')
+var hoodieServer = require('@hoodie/server').register
 
-var getConfig = require('./config')
 var registerPlugins = require('./plugins')
-var userDatabases = require('./utils/user-databases')
 
 function register (server, options, next) {
-  getConfig(options, function (error, config) {
+  server.register({
+    register: hoodieServer,
+    options: options
+  }, function (error) {
     if (error) {
       return next(error)
     }
@@ -19,16 +21,12 @@ function register (server, options, next) {
       sandbox: 'plugin'
     })
 
-    registerPlugins(server, config, function (error) {
+    registerPlugins(server, options, function (error) {
       if (error) {
         return next(error)
       }
 
-      // add / remove user databases on signups / account deletions
-      server.plugins.account.api.accounts.on('add', userDatabases.add.bind(null, config, server))
-      server.plugins.account.api.accounts.on('remove', userDatabases.remove.bind(null, config, server))
-
-      next(null, server, config)
+      next(null, server, options)
     })
   })
 }
